16c79eb696faa2fe2224389601d2683dc53de595
Wei Wang <weiwan@nvidia.com>
2025-05-12 17:23:14 -0700

    Starting from CUDA 12.6 distributed jobs, switch from ubuntu 20.04 to ubuntu 22.04.

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -109,6 +109,17 @@ case "$tag" in
     UCC_COMMIT=${_UCC_COMMIT}
     TRITON=yes
     ;;
+  pytorch-linux-jammy-cuda12.6-cudnn9-py3-gcc11)
+    CUDA_VERSION=12.6.3
+    CUDNN_VERSION=9
+    ANACONDA_PYTHON_VERSION=3.10
+    GCC_VERSION=11
+    VISION=yes
+    KATEX=yes
+    UCX_COMMIT=${_UCX_COMMIT}
+    UCC_COMMIT=${_UCC_COMMIT}
+    TRITON=yes
+    ;;
   pytorch-linux-focal-cuda12.4-cudnn9-py3-gcc9-inductor-benchmarks)
     CUDA_VERSION=12.4.1
     CUDNN_VERSION=9
