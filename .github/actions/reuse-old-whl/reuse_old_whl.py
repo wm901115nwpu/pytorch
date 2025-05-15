dd99c87dffedfb9408ef028e450772c322278913
Catherine Lee <csl@fb.com>
2025-05-15 11:33:23 -0700

    tc

--- .github/actions/reuse-old-whl/reuse_old_whl.py
+++ .github/actions/reuse-old-whl/reuse_old_whl.py
@@ -1,14 +1,15 @@
 import argparse
 import os
-import stat
 import subprocess
-import sys
-import zipfile
 from functools import lru_cache
 from pathlib import Path
+import sys
 from typing import Any
 
 import requests
+
+
+FORCE_REBUILD_LABEL = "ci-force-rebuild"
 
 
 @lru_cache
@@ -21,7 +22,28 @@ def get_merge_base() -> str:
     return merge_base
 
 
+@lru_cache
+def get_head_sha() -> str:
+    sha = subprocess.check_output(
+        ["git", "rev-parse", "HEAD"],
+        text=True,
+        stderr=subprocess.DEVNULL,
+    ).strip()
+    return sha
+
+
+def query_github_api(url: str) -> Any:
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}",
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
 def ok_changed_file(file: str) -> bool:
+    # Return true if the file is in the list of allowed files to be changed to
+    # reuse the old whl
     if (
         file.startswith("torch/")
         and file.endswith(".py")
@@ -33,7 +55,32 @@ def ok_changed_file(file: str) -> bool:
     return False
 
 
+def is_main_branch() -> bool:
+    print(
+        f"Checking if we are on main branch: merge base {get_merge_base()}, head {get_head_sha()}"
+    )
+    return get_merge_base() == get_head_sha()
+
+
+def check_labels_for_pr() -> bool:
+    # Check if the current commit is part of a PR and if it has the
+    # FORCE_REBUILD_LABEL
+    head_sha = get_head_sha()
+    url = f"https://api.github.com/repos/pytorch/pytorch/commits/{head_sha}/pulls"
+    response = query_github_api(url)
+    print(f"Found {len(response)} PRs for commit {head_sha}")
+    for pr in response:
+        labels = pr.get("labels", [])
+        for label in labels:
+            if label["name"] == FORCE_REBUILD_LABEL:
+                print(f"Found label {FORCE_REBUILD_LABEL} in PR {pr['number']}.")
+                return True
+    return False
+
+
 def check_changed_files() -> bool:
+    # Return true if all the changed files are in the list of allowed files to
+    # be changed to reuse the old whl
     merge_base = get_merge_base()
     changed_files = (
         subprocess.check_output(
@@ -54,16 +101,8 @@ def check_changed_files() -> bool:
     return True
 
 
-def query_github_api(url: str) -> Any:
-    headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}",
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-
 def find_old_whl(workflow_id: str, build_environment: str) -> bool:
+    # Find the old whl on s3 and download it to artifacts.zip
     if build_environment is None:
         print("BUILD_ENVIRONMENT is not set.")
         return False
@@ -81,11 +120,11 @@ def find_old_whl(workflow_id: str, build_environment: str) -> bool:
         run_id = run["id"]
         try:
             url = f"https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/{run_id}/{build_environment}/artifacts.zip"
+            print(f"Checking for old whl at {url}")
             response = requests.get(
                 url,
             )
             if response.status_code == 200:
-                os.makedirs("/tmp", exist_ok=True)
                 with open("artifacts.zip", "wb") as f:
                     f.write(response.content)
                     print(f"Found old whl file from s3: {url}")
@@ -94,12 +133,6 @@ def find_old_whl(workflow_id: str, build_environment: str) -> bool:
             print(f"Error checking for old whl: {e}")
             continue
     return False
-
-
-def is_executable(zip_info: zipfile.ZipInfo) -> bool:
-    # Extract UNIX file mode from external_attr
-    mode = (zip_info.external_attr >> 16) & 0xFFFF
-    return bool(mode & stat.S_IXUSR)
 
 
 def unzip_artifact_and_replace_files() -> None:
@@ -151,6 +184,14 @@ def unzip_artifact_and_replace_files() -> None:
     return None
 
 
+def set_output() -> None:
+    if os.getenv("GITHUB_OUTPUT"):
+        with open(str(os.getenv("GITHUB_OUTPUT")), "a") as env:
+            print("reuse=true", file=env)
+    else:
+        print("::set-output name=reuse::true")
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Check for old whl files.")
     parser.add_argument("--workflow-id", type=str, required=True, help="Workflow ID")
@@ -162,13 +203,14 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args = parse_args()
-    can_use_old_whl = check_changed_files()
+    can_use_old_whl = (
+        not is_main_branch() and check_changed_files() and not check_labels_for_pr()
+    )
+    # if not can_use_old_whl:
+    #     print(f"Cannot use old whl, because we are on main branch or changed files.")
+    #     set_build_output()
     if not find_old_whl(args.workflow_id, args.build_environment):
         print("No old whl found.")
-        if os.getenv("GITHUB_OUTPUT"):
-            with open(str(os.getenv("GITHUB_OUTPUT")), "a") as env:
-                print("build=true", file=env)
-        else:
-            print("::set-output name=build::true")
         sys.exit(0)
     unzip_artifact_and_replace_files()
+    set_output()
