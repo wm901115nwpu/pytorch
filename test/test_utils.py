7698edaa6ce495ecfc955186304b1b617c1c2512
Catherine Lee <csl@fb.com>
2025-05-16 15:15:31 -0700

    tc

--- test/test_utils.py
+++ test/test_utils.py
@@ -218,7 +218,6 @@ class TestCheckpoint(TestCase):
                 )
 
     def test_checkpoint_module_list(self):
-        self.assertEqual(1, 2)
         class ModuleListNet(nn.Module):
             def __init__(self) -> None:
                 super().__init__()
